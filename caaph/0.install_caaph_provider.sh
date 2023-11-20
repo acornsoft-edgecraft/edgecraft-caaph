e1d5f9fbda179c7735deb1dd8dfe762c92b17d26
dongmook <dongmook@acornsoft.io>
2023-11-20 18:24:12 +0900

    update caaph installation

--- caaph/0.install_caaph_provider.sh
+++ caaph/0.install_caaph_provider.sh
@@ -2,3 +2,5 @@
 
 kubectl apply -f ./caaph-manager-crds.yaml
 kubectl apply -f ./caaph-manager-deploy.yaml
+kubectl apply -f ./addons.cluster.x-k8s.io_helmchartproxies.yaml
+kubectl apply -f ./addons.cluster.x-k8s.io_helmreleaseproxies.yaml
